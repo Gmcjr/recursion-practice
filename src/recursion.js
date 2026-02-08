0459b7fd296836f199ace68212dbddb69a11bb60
Gmcjr <gmcostajr@gmail.com>
2026-02-07 21:12:55 -0600

    desribe work

--- src/recursion.js
+++ src/recursion.js
@@ -46,8 +46,10 @@ let rest = array.slice(1);
 
 // 4. Check if a number is even.
 var isEven = function(n) {
-  
-};
+  if (n === 0) return true;
+  if (n === 1) return false;
+  return isEven(n - 2);
+}
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
